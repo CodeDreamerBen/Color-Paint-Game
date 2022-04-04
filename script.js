3a9c137492f673b61e59c0d7acfdc554918db12f
CodeDreamerBen <91576401+CodeDreamerBen@users.noreply.github.com>
2022-04-04 08:12:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,295 @@
+var one = document.getElementById("one");
+			var two = document.getElementById("two");
+			var three = document.getElementById("three");
+			var four = document.getElementById("four");
+			var five = document.getElementById("five");
+			var six = document.getElementById("six");
+			var seven = document.getElementById("seven");
+			var eight = document.getElementById("eight");
+			var nine = document.getElementById("nine");
+			var controllerOne = 1;
+			var controllerTwo = 1;
+			var controllerThree = 1;
+			var controllerFour = 1;
+			var controllerFive = 1;
+			var controllerSix = 1;
+			var controllerSeven = 1;
+			var controllerEight = 1;
+			var controllerNine = 1;
+			var bone = document.getElementById("bone");
+			var btwo = document.getElementById("btwo");
+			var bthree = document.getElementById("bthree");
+			var bfour = document.getElementById("bfour");
+			var bfive = document.getElementById("bfive");
+			var bsix = document.getElementById("bsix");
+			var bseven = document.getElementById("bseven");
+			var beight = document.getElementById("beight");
+			var bnine = document.getElementById("bnine");
+			function changeOne() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerOne) {
+					case 0:
+						one.style = "background-color: red;";
+						controllerOne++;
+						break;
+					case 1:
+						one.style = "background-color: yellow;";
+						controllerOne++;
+						break;
+					case 2:
+						one.style = "background-color: lightgreen;";
+						controllerOne++;
+						break;
+					case 3:
+						one.style = "background-color: lightblue;";
+						controllerOne++;
+						break;
+					case 4:
+						one.style = "background-color: purple;";
+						controllerOne = 0;
+						break;
+				}
+			}
+			function changeTwo() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerTwo) {
+					case 0:
+						two.style = "background-color: red;";
+						controllerTwo++;
+						break;
+					case 1:
+						two.style = "background-color: yellow;";
+						controllerTwo++;
+						break;
+					case 2:
+						two.style = "background-color: lightgreen;";
+						controllerTwo++;
+						break;
+					case 3:
+						two.style = "background-color: lightblue;";
+						controllerTwo++;
+						break;
+					case 4:
+						two.style = "background-color: purple;";
+						controllerTwo = 0;
+						break;
+				}
+			}
+			function changeThree() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerThree) {
+					case 0:
+						three.style = "background-color: red;";
+						controllerThree++;
+						break;
+					case 1:
+						three.style = "background-color: yellow;";
+						controllerThree ++;
+						break;
+					case 2:
+						three.style = "background-color: lightgreen;";
+						controllerThree ++;
+						break;
+					case 3:
+						three.style = "background-color: lightblue;";
+						controllerThree ++;
+						break;
+					case 4:
+						three.style = "background-color: purple;";
+						controllerThree = 0;
+						break;
+				}
+			}
+			function changeFour() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerFour) {
+					case 0:
+						four.style = "background-color: red;";
+						controllerFour ++;
+						break;
+					case 1:
+						four.style = "background-color: yellow;";
+						controllerFour ++;
+						break;
+					case 2:
+						four.style = "background-color: lightgreen;";
+						controllerFour ++;
+						break;
+					case 3:
+						four.style = "background-color: lightblue;";
+						controllerFour ++;
+						break;
+					case 4:
+						four.style = "background-color: purple;";
+						controllerFour = 0;
+						break;
+				}
+			}
+			function changeFive() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerFive) {
+					case 0:
+						five.style = "background-color: red;";
+						controllerFive ++;
+						break;
+					case 1:
+						five.style = "background-color: yellow;";
+						controllerFive ++;
+						break;
+					case 2:
+						five.style = "background-color: lightgreen;";
+						controllerFive ++;
+						break;
+					case 3:
+						five.style = "background-color: lightblue;";
+						controllerFive ++;
+						break;
+					case 4:
+						five.style = "background-color: purple;";
+						controllerFive = 0;
+						break;
+				}
+			}
+			function changeSix() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerSix) {
+					case 0:
+						six.style = "background-color: red;";
+						controllerSix ++;
+						break;
+					case 1:
+						six.style = "background-color: yellow;";
+						controllerSix ++;
+						break;
+					case 2:
+						six.style = "background-color: lightgreen;";
+						controllerSix ++;
+						break;
+					case 3:
+						six.style = "background-color: lightblue;";
+						controllerSix ++;
+						break;
+					case 4:
+						six.style = "background-color: purple;";
+						controllerSix = 0;
+						break;
+				}
+			}
+			function changeSeven() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerSeven) {
+					case 0:
+						seven.style = "background-color: red;";
+						controllerSeven ++;
+						break;
+					case 1:
+						seven.style = "background-color: yellow;";
+						controllerSeven ++;
+						break;
+					case 2:
+						seven.style = "background-color: lightgreen;";
+						controllerSeven ++;
+						break;
+					case 3:
+						seven.style = "background-color: lightblue;";
+						controllerSeven ++;
+						break;
+					case 4:
+						seven.style = "background-color: purple;";
+						controllerSeven = 0;
+						break;
+				}
+			}
+			function changeEight() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerEight) {
+					case 0:
+						eight.style = "background-color: red;";
+						controllerEight ++;
+						break;
+					case 1:
+						eight.style = "background-color: yellow;";
+						controllerEight ++;
+						break;
+					case 2:
+						eight.style = "background-color: lightgreen;";
+						controllerEight ++;
+						break;
+					case 3:
+						eight.style = "background-color: lightblue;";
+						controllerEight ++;
+						break;
+					case 4:
+						eight.style = "background-color: purple;";
+						controllerEight = 0;
+						break;
+				}
+			}
+			function changeNine() {
+				var newNum = Number(document.getElementById("color").value)
+				document.getElementById("color").value = newNum + 1
+				switch(controllerNine) {
+					case 0:
+						nine.style = "background-color: red;";
+						controllerNine ++;
+						break;
+					case 1:
+						nine.style = "background-color: yellow;";
+						controllerNine ++;
+						break;
+					case 2:
+						nine.style = "background-color: lightgreen;";
+						controllerNine ++;
+						break;
+					case 3:
+						nine.style = "background-color: lightblue;";
+						controllerNine ++;
+						break;
+					case 4:
+						nine.style = "background-color: purple;";
+						controllerNine = 0;
+						break;
+				}
+			}
+			function check() {
+				if(one.style.backgroundColor == bone.style.backgroundColor && two.style.backgroundColor == btwo.style.backgroundColor && three.style.backgroundColor == bthree.style.backgroundColor && four.style.backgroundColor == bfour.style.backgroundColor && five.style.backgroundColor == bfive.style.backgroundColor && six.style.backgroundColor == bsix.style.backgroundColor && seven.style.backgroundColor == bseven.style.backgroundColor && eight.style.backgroundColor == beight.style.backgroundColor && nine.style.backgroundColor == bnine.style.backgroundColor) {
+					var newNum = Number(document.getElementById("completed").value)
+					document.getElementById("completed").value = newNum + 1;
+					var prompts = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+					var a = prompts[Math.floor(Math.random() * prompts.length)]
+					var b = prompts[Math.floor(Math.random() * prompts.length)]
+					var c = prompts[Math.floor(Math.random() * prompts.length)]
+					var d = prompts[Math.floor(Math.random() * prompts.length)]
+					var e = prompts[Math.floor(Math.random() * prompts.length)]
+					var f = prompts[Math.floor(Math.random() * prompts.length)]
+					var hex = `#${a}${b}${c}${d}${e}${f}`
+					document.bgColor = hex;
+					var promptTwo = ["red", "yellow", "lightgreen", "lightblue", "purple"];
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					bone.style.backgroundColor = color;
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					btwo.style.backgroundColor = color;
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					bthree.style.backgroundColor = color;
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					bfour.style.backgroundColor = color;
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					bfive.style.backgroundColor = color;
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					bsix.style.backgroundColor = color;
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					bseven.style.backgroundColor = color;
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					beight.style.backgroundColor = color;
+					var color = promptTwo[Math.floor(Math.random() * promptTwo.length)];
+					bnine.style.backgroundColor = color;
+				}
+			}
